9b4a2ba53c25b7f9a33b4d622e4410049228a207
Fuad Trola <fudtroll@gmail.com>
2024-08-08 23:12:06 +0700

    New folder

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,21 +1,21 @@
 (function () {
   'use strict';
 
-  angular.module('LunchCheck',[])
-  .controller('LunchController', LunchController);
-  LunchController.$inject = ['$scope'];
-  function LunchController($scope) {
-    $scope.lunchMenu = "";
-    $scope.resultText = "";
-    $scope.resultClass = "";
+  angular
+    .module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController)
+  LunchCheckController.$inject = ['$scope']
+  function LunchCheckController($scope) {
+    $scope.lunchMenu = ''
+    $scope.resultText = ''
+    $scope.resultClass = ''
     $scope.checkLunch = function () {
       var menuListed = $scope.lunchMenu.split(',')
-      menuListed = menuListed.filter(menuListed => menuListed.trim() !=='')
+      menuListed = menuListed.filter((menuListed) => menuListed.trim() !== '')
       var menuLength = menuListed.length
       if (menuLength <= 0) {
         $scope.resultClass = 'red-box'
         $scope.resultText = 'Please enter data first'
-
       } else {
         $scope.resultClass = 'green-box'
         if (menuLength > 0 && menuLength <= 3) {
